d391a0a1b152a7adfa037e372de4395139eb8a5b
eshsen <darsyeva@mail.ru>
2025-09-09 17:17:03 +0700

    Update cv-1-12.py

--- cv-1-12.py
+++ cv-1-12.py
@@ -4,16 +4,13 @@ import numpy as np
 
 def pixel_counting(image_bgr):
     image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
-
     lower_red1 = np.array([0, 100, 100])
     upper_red1 = np.array([10, 255, 255])
     lower_red2 = np.array([170, 100, 100])
     upper_red2 = np.array([180, 255, 255])
-
     mask1 = cv2.inRange(image_hsv, lower_red1, upper_red1)
     mask2 = cv2.inRange(image_hsv, lower_red2, upper_red2)
     mask = cv2.bitwise_or(mask1, mask2)
-
     red_pixel_count = int(np.count_nonzero(mask))
     return red_pixel_count
 
@@ -22,4 +19,5 @@ if __name__ == "__main__":
     image_path = 'img.png'
     image_bgr = cv2.imread(image_path)
     print(pixel_counting(image_bgr))
+
 
