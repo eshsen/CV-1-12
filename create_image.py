514589da2c6dfca2178295619092614379ca7135
eshsen <darsyeva@mail.ru>
2025-09-09 17:17:49 +0700

    Update create_image.py

--- create_image.py
+++ create_image.py
@@ -1,16 +1,13 @@
 from PIL import Image
 import random
 
-N = 189
-M = 761
-
+N = int(input())
+M = int(input())
 img = Image.new('RGB', (N, M))
-
 for x in range(N):
     for y in range(M):
         r = random.randint(200, 255)
         g = random.randint(0, 50)
         b = random.randint(0, 50)
         img.putpixel((x, y), (r, g, b))
-
 img.save('img.png')
